3412cc5c4e27b78129bcd99dfbb6ef5d5cd3cbd6
Adina Stoica <adina.stoica@gmail.com>
2024-12-16 23:50:06 -0500

    Manage showing the right screen based on state

--- src/components/BroadwayLottery.tsx
+++ src/components/BroadwayLottery.tsx
@@ -14,10 +14,15 @@ import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 import { LOTTERY_LIST } from "../utils/constants";
 import { ProfilesContext } from "../utils/ProfilesContext";
+import { Circle } from "@mui/icons-material";
 
 export const BroadwayLottery: React.FC = () => {
-    const { profiles, setProfiles, setNotification, setEditingIndex } =
-        useContext(ProfilesContext);
+    const {
+        showLotteryList,
+        setShowLotteryList,
+        setShowSavedProfiles,
+        setShowProfileForm,
+    } = useContext(ProfilesContext);
 
     const [selectedShows, setSelectedShows] = useState<number[]>([]);
     const [allSelected, setAllSelected] = useState<boolean>(false);
@@ -46,20 +51,67 @@ export const BroadwayLottery: React.FC = () => {
     const handleOpenSelected = (): void => {
         LOTTERY_LIST.filter((show) => selectedShows.includes(show.id)).forEach(
             (show) => {
-                chrome.tabs.create(
-                    {
-                        url: `${show.url}?bwayExt=true`,
-                        active: false,
-                    },
-                    (tab) => {}
-                );
+                chrome.tabs.create({
+                    url: `${show.url}?bwayExt=true`,
+                    active: false,
+                });
             }
         );
     };
 
+    const buttonRow = (
+        <div style={{ marginTop: "20px", textAlign: "center" }}>
+            <Button
+                variant="contained"
+                color={!allSelected ? "success" : "warning"}
+                startIcon={!allSelected ? <CheckCircleIcon /> : <Circle />}
+                onClick={handleSelectAll}
+                style={{ marginRight: "10px", width: "165px" }}
+            >
+                {!allSelected ? "Select All" : "Unselect All"}
+            </Button>
+            <Button
+                variant="contained"
+                color="primary"
+                startIcon={<OpenInNewIcon />}
+                onClick={handleOpenSelected}
+                disabled={selectedShows.length === 0}
+            >
+                Open Selected
+            </Button>
+        </div>
+    );
+
+    const handleSeeProfilesClick = (): void => {
+        setShowLotteryList(false);
+        setShowSavedProfiles(true);
+        setShowProfileForm(false);
+    };
+
+    if (!showLotteryList) {
+        return <></>;
+    }
+
     return (
         <Container>
-            <h1 id="lotteries">Broadway Direct Lotteries</h1>
+            <div
+                style={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                }}
+            >
+                <h1 id="lotteries">Broadway Direct Lotteries</h1>
+                <Button
+                    variant="contained"
+                    color="secondary"
+                    onClick={handleSeeProfilesClick}
+                    style={{ marginLeft: "auto" }}
+                >
+                    See Profiles
+                </Button>
+            </div>
+            {buttonRow}
             <List>
                 {LOTTERY_LIST.map((show) => (
                     <ListItem key={show.id} divider>
@@ -79,26 +131,7 @@ export const BroadwayLottery: React.FC = () => {
                     </ListItem>
                 ))}
             </List>
-            <div style={{ marginTop: "20px", textAlign: "center" }}>
-                <Button
-                    variant="contained"
-                    color="primary"
-                    startIcon={<CheckCircleIcon />}
-                    onClick={handleSelectAll}
-                    style={{ marginRight: "10px" }}
-                >
-                    {!allSelected ? "Select All" : "Unselect All"}
-                </Button>
-                <Button
-                    variant="contained"
-                    color="secondary"
-                    startIcon={<OpenInNewIcon />}
-                    onClick={handleOpenSelected}
-                    disabled={selectedShows.length === 0}
-                >
-                    Open Selected
-                </Button>
-            </div>
+            {buttonRow}
         </Container>
     );
 };
